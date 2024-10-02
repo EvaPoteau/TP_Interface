cbc4b7d988d1d479c2dc220c879aa446b4816b2b
EvaPoteau <160507792+EvaPoteau@users.noreply.github.com>
2024-10-02 11:48:12 +0200

    Add files via upload

---
+++ Exemple Interface/Exemple Interface/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace ExempleInterface
+{
+    interface IExemple
+    {
+        string nom { get; set; }
+        string NomMajuscule();
+    }
+    public class Exemple : IExemple
+    {
+        public string nom { get; set; }
+        public Exemple(string nom)
+        {
+            this.nom = nom;
+        }
+        public string NomMajuscule()
+        {
+            return this.nom.ToUpper();
+        }
+    }
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            IExemple exemple = new Exemple("toto");
+
+        }
+    }
+}
